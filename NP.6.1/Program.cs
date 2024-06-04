af89007476a3aee2287f4c14c385db38d01e9f1b
Паша Ярошенко <pavlo.legominecraft@gmail.com>
2024-06-04 08:21:28 +0300

    6.1

--- NP.6.1/Program.cs
+++ NP.6.1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,20 +11,33 @@ namespace NP._6._1
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
             while (true)
             {
-                Console.WriteLine("Таблиця try");
+                int cookie = 0;
+                Console.WriteLine("Дайте відповідь");
                 Random random = new Random();
                 int[] massX = new int [5]{random.Next(0,11), random.Next(0, 11), random.Next(0, 11), random.Next(0, 11), random.Next(0, 11) };
                 int[] massY = new int[5] { random.Next(0, 11), random.Next(0, 11), random.Next(0, 11), random.Next(0, 11), random.Next(0, 11)};
 
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine($"Кількість правильни {cookie} із 5");
+                    Console.Write($"{massX[i]}*{massY[i]}=");
+                    int current = Convert.ToInt32(Console.ReadLine());
+                    if(current != massX[i] * massY[i]) {
+                        Console.Clear();
+                        break;
+                    }
+                    cookie++;
+                    Console.Clear();
+
+                }
+                if(cookie == 5)
+                {
+                    Console.WriteLine("Ви відповіли на всі запитання ");
+                }
             }
         }
-        static void DrawTable(int[] massX,int[] massY)
-        {
-            for (int i = 0; i < 5; i++)
-            {
-                Console.WriteLine(massX[i] + "*" + massY[i]);
-            }        }
     }
 }
