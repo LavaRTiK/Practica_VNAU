2e4860d33b1316851deb28c183e6d70246974d96
Паша Ярошенко <pavlo.legominecraft@gmail.com>
2024-06-06 10:37:49 +0300

    9.4

--- NP.9.4F/Form1.Designer.cs
+++ NP.9.4F/Form1.Designer.cs
@@ -28,12 +28,194 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            label5 = new Label();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 11F);
+            label1.Location = new Point(72, 11);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(437, 30);
+            label1.TabIndex = 1;
+            label1.Text = "Показнаки за споживання елеетроенергії";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(34, 79);
+            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(190, 25);
+            label2.TabIndex = 2;
+            label2.Text = "Попередні показники";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(34, 139);
+            label3.Margin = new Padding(4, 0, 4, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(169, 25);
+            label3.TabIndex = 3;
+            label3.Text = "Поточні показники";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(34, 194);
+            label4.Margin = new Padding(4, 0, 4, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(74, 25);
+            label4.TabIndex = 4;
+            label4.Text = "Різниця";
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(31, 289);
+            checkBox1.Margin = new Padding(4);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(167, 29);
+            checkBox1.TabIndex = 5;
+            checkBox1.Text = "Інша ціна за од.";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(34, 385);
+            checkBox2.Margin = new Padding(4);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(149, 29);
+            checkBox2.TabIndex = 6;
+            checkBox2.Text = "Підтверджую";
+            checkBox2.UseVisualStyleBackColor = true;
+            checkBox2.CheckedChanged += checkBox2_CheckedChanged;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(31, 510);
+            label5.Margin = new Padding(4, 0, 4, 0);
+            label5.Name = "label5";
+            label5.Size = new Size(93, 25);
+            label5.TabIndex = 7;
+            label5.Text = "До сплати";
+            // 
+            // button1
+            // 
+            button1.Enabled = false;
+            button1.Location = new Point(326, 354);
+            button1.Margin = new Padding(4);
+            button1.Name = "button1";
+            button1.Size = new Size(246, 90);
+            button1.TabIndex = 8;
+            button1.Text = "Обрахунок";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(392, 75);
+            textBox1.Margin = new Padding(4);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(190, 31);
+            textBox1.TabIndex = 9;
+            textBox1.TextChanged += textBox1_TextChanged;
+            textBox1.KeyPress += textBox1_KeyPress;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(392, 135);
+            textBox2.Margin = new Padding(4);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(190, 31);
+            textBox2.TabIndex = 10;
+            textBox2.KeyPress += textBox2_KeyPress;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(392, 190);
+            textBox3.Margin = new Padding(4);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(190, 31);
+            textBox3.TabIndex = 11;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(416, 506);
+            textBox4.Margin = new Padding(4);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(155, 31);
+            textBox4.TabIndex = 12;
+            // 
+            // textBox5
+            // 
+            textBox5.Enabled = false;
+            textBox5.Location = new Point(392, 286);
+            textBox5.Margin = new Padding(4);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(179, 31);
+            textBox5.TabIndex = 13;
+            textBox5.TextChanged += textBox5_TextChanged;
+            textBox5.KeyPress += textBox5_KeyPress;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(612, 562);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Controls.Add(label5);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Margin = new Padding(4);
+            Name = "Form1";
+            Text = " ";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private Label label5;
+        private Button button1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
     }
 }
--- NP.9.4F/Form1.cs
+++ NP.9.4F/Form1.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+
 namespace NP._9._4F
 {
     public partial class Form1 : Form
@@ -5,6 +7,132 @@ namespace NP._9._4F
         public Form1()
         {
             InitializeComponent();
+        }
+        double price = 4.25;
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsDigit(e.KeyChar) == true)
+            {
+                cheack();
+                return;
+            }
+            else if (char.IsControl(e.KeyChar) == true)
+            {
+                cheack();
+                return;
+            }
+            else if (e.KeyChar == ',' && textBox1.Text.Count(x => x == ',') < 1)
+            {
+                return;
+            }
+            else
+            {
+                cheack();
+                e.Handled = true;
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsDigit(e.KeyChar) == true)
+            {
+                cheack();
+                return;
+            }
+            else if (char.IsControl(e.KeyChar) == true)
+            {
+                cheack();
+                return;
+            }
+            else if (e.KeyChar == ',' && textBox2.Text.Count(x => x == ',') < 1)
+            {
+                return;
+            }
+            else
+            {
+                cheack();
+                e.Handled = true;
+            }
+        }
+
+        private void textBox5_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsDigit(e.KeyChar) == true)
+            {
+
+                return;
+            }
+            else if (char.IsControl(e.KeyChar) == true)
+            {
+                return;
+            }
+            else if (e.KeyChar == ',' && textBox5.Text.Count(x => x == ',') < 1)
+            {
+                return;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked == true)
+            {
+                button1.Enabled = true;
+                textBox1.Enabled = false;
+                textBox2.Enabled = false;
+                textBox3.Enabled = false;
+            }
+            else
+            {
+                button1.Enabled = false;
+                textBox1.Enabled = true;
+                textBox2.Enabled = true;
+                textBox3.Enabled = true;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void cheack()
+        {
+            if (!string.IsNullOrWhiteSpace(textBox1.Text) && !string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                textBox3.Text = (Convert.ToDouble(textBox1.Text) - Convert.ToDouble(textBox2.Text)).ToString();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox4.Text = (Convert.ToDouble(textBox2.Text) * price).ToString();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                textBox5.Enabled = true;
+            }
+            else
+            {
+                textBox5.Enabled = false;
+                price = 4.25;
+            }
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            price = Convert.ToDouble(textBox5.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
