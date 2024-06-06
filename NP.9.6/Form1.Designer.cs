2e7108bd4bc94fcec179a4faca879041c90c067c
Паша Ярошенко <pavlo.legominecraft@gmail.com>
2024-06-06 19:44:51 +0300

    9.6

--- NP.9.6/Form1.Designer.cs
+++ NP.9.6/Form1.Designer.cs
@@ -28,19 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            label2 = new Label();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            button1 = new Button();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(17, 28);
+            label1.Name = "label1";
+            label1.Size = new Size(70, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Ширина,см";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(17, 57);
+            label2.Name = "label2";
+            label2.Size = new Size(66, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Висота, см";
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(17, 100);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(69, 19);
+            radioButton1.TabIndex = 2;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "пластик";
+            radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.CheckedChanged += radioButton1_CheckedChanged;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(17, 125);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(58, 19);
+            radioButton2.TabIndex = 3;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "метал";
+            radioButton2.UseVisualStyleBackColor = true;
+            radioButton2.CheckedChanged += radioButton2_CheckedChanged;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(17, 184);
+            label3.Name = "label3";
+            label3.Size = new Size(63, 15);
+            label3.TabIndex = 4;
+            label3.Text = "До сплати";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(192, 20);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(119, 23);
+            textBox1.TabIndex = 5;
+            textBox1.TextChanged += textBox1_TextChanged;
+            textBox1.KeyPress += textBox1_KeyPress;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(192, 54);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(119, 23);
+            textBox2.TabIndex = 6;
+            textBox2.TextChanged += textBox2_TextChanged;
+            textBox2.KeyPress += textBox2_KeyPress;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(192, 181);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(119, 23);
+            textBox3.TabIndex = 7;
+            // 
+            // button1
+            // 
+            button1.Enabled = false;
+            button1.Location = new Point(175, 100);
+            button1.Name = "button1";
+            button1.Size = new Size(136, 44);
+            button1.TabIndex = 8;
+            button1.Text = "РОЗРАХУНОК";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(339, 253);
+            Controls.Add(button1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Metal";
             Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button button1;
     }
 }
--- NP.9.6/Form1.cs
+++ NP.9.6/Form1.cs
@@ -11,5 +11,93 @@ namespace NP._9._6
         {
 
         }
+        private double price = 15.5;
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsDigit(e.KeyChar) == true)
+            {
+
+                return;
+            }
+            else if (char.IsControl(e.KeyChar) == true)
+            {
+                return;
+            }
+            else if (e.KeyChar == ',' && textBox1.Text.Count(x => x == ',') < 1)
+            {
+                return;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsDigit(e.KeyChar) == true)
+            {
+
+                return;
+            }
+            else if (char.IsControl(e.KeyChar) == true)
+            {
+                return;
+            }
+            else if (e.KeyChar == ',' && textBox2.Text.Count(x => x == ',') < 1)
+            {
+                return;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+        private void cheak()
+        {
+            if (radioButton1.Checked && !string.IsNullOrWhiteSpace(textBox1.Text) && !string.IsNullOrWhiteSpace(textBox2.Text) || radioButton2.Checked && !string.IsNullOrWhiteSpace(textBox1.Text) && !string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+
+                button1.Enabled = true;
+            }
+            else
+            {
+                button1.Enabled = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = (Math.Round(Convert.ToInt32(textBox1.Text) * Convert.ToInt32(textBox2.Text) * price),2).ToString(); 
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            cheak();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            cheak();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            cheak();
+            if(radioButton1.Checked)
+            {
+                price = 15.5;
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            cheak();
+            if(radioButton2.Checked)
+            {
+                price = 30.15;
+            }
+            
+        }
     }
 }
